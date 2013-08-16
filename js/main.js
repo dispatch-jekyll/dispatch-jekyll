db0c2b70a077c04680133dc7dd90bd546d28f6c3
Jesse Shawl <jesse@jshawl.com>
2013-08-15 21:07:32 -0400

    cleaned up markup, templates, and js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,51 @@
-$('input[data-update]').bind('keyup',function(){
-	$('.js-update').html($(this).val()); 
-});
+;(function(){
+  var bindUI = {
+    init: function(){
+      this.verbutton();
+      this.contenteditable();
+      this.delete();
+    },
+    verbutton : function(){
+      $('.verbutton').bind('click', function(event){
+        event.preventDefault(); 
+        $(this).toggleClass('active');
+        var _html = "Publish";
+        var span = $(this).find('span');
+        if($(this).hasClass('active')){
+          span.html(_html+'ed');
+        } else{
+          span.html(_html);
+        }
+        return false;
+      });
+    },
+    contenteditable:function(){
+      $('[contenteditable]').bind('keyup', function(){
+        contentEditor.create();
+      });
+    },
+    delete:function(){
+      $('.attn').bind('click', function(){
+        contentEditor.delete();
+      });
+    }
+  };
+  var contentEditor = {
+    create:function(){
+      localStorage['data'] = JSON.stringify($('[contenteditable]').html());
+    },
+    read:function(){
+      if(localStorage['data']){
+        $('[contenteditable]').html(JSON.parse(localStorage['data']));
+      }
+    },
+    delete:function(){
+      localStorage['data'] = '';
+    }
+  };
+  $(function(){
+    bindUI.init();
+    contentEditor.read();
+  });
+})(jQuery);
 
-//console.log($('input[data-update]'));  
-
-$(function(){
-		$('.verbutton').bind('click', function(event){
-  event.preventDefault(); 
-  $(this).toggleClass('active');
-  var _html = "Publish";
-  var span = $(this).find('span');
-  if($(this).hasClass('active')){
-    span.html(_html+'ed');
-  } else{
-    span.html(_html);
-  }
-  
-  return false;
-});
-	});
-
-$(function(){
-	if(localStorage['data']){
-		$('[contenteditable]').html(JSON.parse(localStorage['data']));
-	}
-});
-//some local storage stuff
-$('[contenteditable]').bind('keyup', function(){
-	localStorage['data'] = JSON.stringify($('[contenteditable]').html());
-	console.log(localStorage);
-});
-
-$('.attn').bind('click', function(){
-	localStorage['data'] = '';
-});
